7ff350efd0d9e4f0e002fc41bec4492d55f081d1
Andrés Lozano <camilolozano850@gmail.com>
2024-06-11 20:29:34 -0500

    Ajuste instalacion npm install mongoose

---
+++ index.js
@@ -0,0 +1,12 @@
+// Importaciones 
+
+// Conexion a la bd
+
+// Configurar los cors: permite que las peticiones se hagan correctamente
+
+// hacer la conversion de datos (body a Objetos JS)
+
+// Configurar las Rutas
+
+//Configurar el servidor para escuchar las peticiones HTTP
+
