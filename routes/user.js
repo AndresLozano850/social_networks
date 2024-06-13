fcca87198f39b94fdcbaca194793a23f376d2546
Andrés Lozano <camilolozano850@gmail.com>
2024-06-12 22:06:12 -0500

    controller user Ok

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 //Importaciones de Express Router Ayuda para la utilizacion de los metodos de express
 import { Router } from "express";
 const router = Router();
-import {testUser} from "../controllers/user.js"
+import {register, testUser} from "../controllers/user.js"
 
-// Definir las rutas
+// Definir las rutas para usuarlas
 
 router.get('/test-user', testUser);
+router.post('/register', register);
 
 // Exportar el Router
 export default router;
