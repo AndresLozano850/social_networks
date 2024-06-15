0f6913a10b17e1f20b66c14dc1e9cb43da5bc454
Andrés Lozano <camilolozano850@gmail.com>
2024-06-14 20:31:02 -0500

    Paginator ok

--- models/user.js
+++ models/user.js
@@ -1,41 +1,44 @@
-import { Schema, model } from "mongoose";
+import { Schema, model} from "mongoose";
+import mongoosePaginate from 'mongoose-paginate-v2';
 
-const UserSchema = Schema({
-// modelo de nuestro escquema
-    name:{
-        type:String,
-        required:true
-    },
-    last_name:{
-        type:String,
-        required:true
-    },
-    nick:{
-        type: String,
-        required: true
-    },
-    bio:String,
-    email:{
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    role:{
-        type: String,
-        default: "role_user"
-    },
-    image:{
-        type: String,
-        default: "dafault.png"
-    },
-    create_at:{
-        type: Date,
-        default: Date.now
-    }
-
+const UserSchema = Schema ({
+  name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  nick: {
+    type: String,
+    required: true
+  },
+  bio: String,
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: "role_user"
+  },
+  image: {
+    type: String,
+    default: "default.png"
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-export default model("User",UserSchema,"users");
+// Añadir pluggin de paginación
+UserSchema.plugin(mongoosePaginate);
+
+
+export default model("User", UserSchema, "users");
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 //Importaciones de Express Router Ayuda para la utilizacion de los metodos de express
 import { Router } from "express";
 const router = Router();
-import {register, testUser, login, profile} from "../controllers/user.js";
+import {register, testUser, login, profile, listUsers} from "../controllers/user.js";
 import {ensureAuth} from "../middlewares/auth.js"; // Middleware
 // Definir las rutas para usuarlas
 
@@ -9,6 +9,6 @@ router.get('/test-user', ensureAuth, testUser);
 router.post('/register', register);
 router.post('/login', login);
 router.get('/profile/:id', ensureAuth, profile);
-
+router.get('/list/:page?', ensureAuth, listUsers);
 // Exportar el Router
 export default router;
