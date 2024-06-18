40a7b4df81e45d1c893b8f00a8161ab4cc5a4675
Andrés Lozano <camilolozano850@gmail.com>
2024-06-17 19:27:44 -0500

    Method update user

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 //Importaciones de Express Router Ayuda para la utilizacion de los metodos de express
 import { Router } from "express";
 const router = Router();
-import {register, testUser, login, profile, listUsers} from "../controllers/user.js";
+import {register, testUser, login, profile, listUsers, updateUser} from "../controllers/user.js";
 import {ensureAuth} from "../middlewares/auth.js"; // Middleware
 // Definir las rutas para usuarlas
 
@@ -10,5 +10,6 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/profile/:id', ensureAuth, profile);
 router.get('/list/:page?', ensureAuth, listUsers);
+router.put('/update', ensureAuth, updateUser);
 // Exportar el Router
 export default router;
