a0f1c767f842cbc354b9a18052829795e9301b53
Andrés Lozano <camilolozano850@gmail.com>
2024-06-20 19:12:58 -0500

    Metodo para dejar de Seguir

--- routes/follow.js
+++ routes/follow.js
@@ -1,11 +1,12 @@
 //Importaciones de Express Router Ayuda para la utilizacion de los metodos de express
 import { Router } from "express";
 const router = Router();
-import { testFollow } from "../controllers/follow.js"
+import { testFollow, saveFollow } from "../controllers/follow.js";
+import { ensureAuth } from "../middlewares/auth.js";
 
 // Definir las rutas
-
 router.get('/test-follow', testFollow);
+router.post("/follow", ensureAuth, saveFollow);
 
 // Exportar el Router
 export default router;
