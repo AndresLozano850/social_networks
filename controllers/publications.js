42a331513ffd2806ed29567ac05ccc364ace528f
Andrés Lozano <camilolozano850@gmail.com>
2024-06-21 20:19:12 -0500

    publications test

--- controllers/publications.js
+++ controllers/publications.js
@@ -1,6 +1,25 @@
+import publication from "../models/publication.js";
+
 // Acciones de prueba
 export const testPublication = (req, res) => {
     return res.status(200).send({
       message: "Mensaje enviado desde el controlador: publications.js"
     });
-  }
+}
+
+// Metodo para hacer una Publicación
+export const savePublication = async(req,res)=>{
+    try {
+        return res.status(200).send({
+            status: "success",
+            message: "!Publicación creada con éxito"
+          });
+        
+    } catch (error) {
+        console.log("Error al crear la publicacion",error)
+        return res.status(500).send({
+            status: "error",
+            message: "Error al crear la publicacion"
+          });
+    }
+}
--- routes/publications.js
+++ routes/publications.js
@@ -1,11 +1,11 @@
 //Importaciones de Express Router Ayuda para la utilizacion de los metodos de express
 import { Router } from "express";
 const router = Router();
-import { testPublication } from "../controllers/publications.js"
-
+import { testPublication,  savePublication } from "../controllers/publications.js"
+import { ensureAuth } from "../middlewares/auth.js";
 // Definir las rutas
 
 router.get('/test-publication', testPublication);
-
+router.post('/publication', ensureAuth, savePublication);
 // Exportar el Router
 export default router;
