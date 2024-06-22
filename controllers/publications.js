4638e27d07e054e357a5f0907729b0b1cba2af3a
Andrés Lozano <camilolozano850@gmail.com>
2024-06-21 20:39:41 -0500

    Publications controllers ok

--- controllers/publications.js
+++ controllers/publications.js
@@ -1,25 +1,56 @@
-import publication from "../models/publication.js";
+import Publication from "../models/publication.js"
 
 // Acciones de prueba
 export const testPublication = (req, res) => {
-    return res.status(200).send({
-      message: "Mensaje enviado desde el controlador: publications.js"
-    });
+  return res.status(200).send({
+    message: "Mensaje enviado desde el controlador: publications.js"
+  });
 }
 
-// Metodo para hacer una Publicación
-export const savePublication = async(req,res)=>{
-    try {
-        return res.status(200).send({
-            status: "success",
-            message: "!Publicación creada con éxito"
-          });
-        
-    } catch (error) {
-        console.log("Error al crear la publicacion",error)
-        return res.status(500).send({
-            status: "error",
-            message: "Error al crear la publicacion"
-          });
+// Método para hacer una publicación
+export const savePublication = async (req, res) => {
+  try {
+    
+    // Obtener datos del body
+    const params = req.body;
+
+    // Verificar que llegue desde el body el parámetro text con su información
+    if (!params.text){
+      return res.status(400).send({
+        status: "error",
+        message: "Debes enviar el texto de la publicación"
+      });
     }
+
+    // Crear el objeto del modelo
+    let newPublication = new Publication(params);
+
+    // Agregar la información del usuario autenticado al objeto de la nueva publicación
+    newPublication.user_id = req.user.userId;
+
+    // Guardar la nueva publicación en la BD
+    const publicationStored = await newPublication.save();
+
+    // Verificar si se guardó la publicación en la BD (si existe publicationStored)
+    if (!publicationStored){
+      return res.status(500).send({
+        status: "error",
+        message: "No se ha guardado la publicación"
+      });
+    }
+
+    // Devolver respuesta exitosa 
+    return res.status(200).send({
+      status: "success",
+      message: "¡Publicación creada con éxito!",
+      publicationStored
+    });
+
+  } catch (error) {
+    console.log("Error al crear la publicación:", error);
+    return res.status(500).send({
+      status: "error",
+      message: "Error al crear la publicación"
+    });
+  }
 }
